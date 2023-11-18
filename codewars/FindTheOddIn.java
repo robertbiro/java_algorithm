08468f01a023630baf42bf088ad3c0e7107e59f7
robertbiro <biro.k.robert@gmail.com>
2023-11-18 18:09:46 +0100

    6 kyu-2023.11.18 -ready, fix, but not serious problem

--- codewars/FindTheOddIn.java
+++ codewars/FindTheOddIn.java
@@ -9,7 +9,7 @@ public class FindTheOddIn {
             for (int i = 0; i < a.length; i++) {
                 int count = 1;
 
-                for(int j = i +1; j < a.length -1; j++) {
+                for(int j = i +1; j < a.length; j++) {
                     if (a[i] == a[j]) {
                         count++;
                     } else {
